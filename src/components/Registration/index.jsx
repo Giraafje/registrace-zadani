c629173a761abfc8b1f310481bc076caa2b45835
Giraafje <lenka.erbenova79@gmail.com>
2023-10-05 18:07:28 +0200

    Adding check on '@' in email.

--- src/components/Registration/index.jsx
+++ src/components/Registration/index.jsx
@@ -23,7 +23,7 @@ const Registration = () => {
  
     return (
         <form onSubmit={handleSubmit}>
-            <input type="email" name="userEmail" placeholder="Email Address" value={user.email} onChange={(e) => setUser({...user, email: e.target.value})} required onBlur={() => setUser({...user, username: user.username === '' ? user.email.split('@')[0] : user.username})}></input>
+            <input type="email" name="userEmail" placeholder="Email Address" value={user.email} onChange={(e) => setUser({...user, email: e.target.value})} required onBlur={() => setUser({...user, username: user.username === '' && user.email.includes('@') ? user.email.split('@')[0] : user.username})}></input>
             <input type="text" name="userName" placeholder="User Name" value={user.username} onChange={(e) => setUser({...user, username: e.target.value})} required></input>
             <input type="password" name="password" placeholder="Password" value={user.password} onChange={(e) => setUser({...user, password: e.target.value})} required></input>
             <input type="password" name="confirmPassword" placeholder="Confirm Password" value={user.passwordConfirm} onChange={(e) => setUser({...user, passwordConfirm: e.target.value})} required></input>
